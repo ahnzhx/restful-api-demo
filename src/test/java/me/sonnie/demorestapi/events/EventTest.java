de81d0b72baee706d167392a474314aad83f68f3
ahnzhx <modunaeggu@naver.com>
2024-10-10 16:33:57 -0400

    build test with parameters

--- src/test/java/me/sonnie/demorestapi/events/EventTest.java
+++ src/test/java/me/sonnie/demorestapi/events/EventTest.java
@@ -1,9 +1,16 @@
 package me.sonnie.demorestapi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 class EventTest {
 
     @Test
@@ -14,51 +21,42 @@ class EventTest {
                 .build();
         assertThat(event).isNotNull();
     }
-    @Test
-    public void testFree(){
+    @ParameterizedTest
+    @MethodSource
+    public void testFree(int basePrice, int maxPrice, boolean isFree){
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         event.update();
 
-        assertThat(event.isFree()).isTrue();
-
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
+    static Object[] testFree(){
+        return new Object[]{
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
+    }
+    @ParameterizedTest
+    @MethodSource
+    public void testOffline(String location, boolean offline){
+        Event event = Event.builder()
+                .location(location)
                 .build();
 
         event.update();
 
-        assertThat(event.isFree()).isFalse();
-
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        event.update();
-
-        assertThat(event.isFree()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(offline);
     }
 
-    @Test
-    public void testOffline(){
-        Event event = Event.builder()
-                .location("ucf library")
-                .build();
-
-        event.update();
-
-        assertThat(event.isOffline()).isTrue();
-
-        event = Event.builder()
-                .build();
-
-        event.update();
-
-        assertThat(event.isOffline()).isFalse();
+    static Object[] testOffline(){
+        return new Object[]{
+                new Object[]{"ucf library", true},
+                new Object[]{null, false}
+        };
     }
 }
